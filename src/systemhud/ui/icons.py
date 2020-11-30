be786f0cd0187fa7bccd6a5e831dc67ba53949f6
Jeff <jdost@users.noreply.github.com>
2020-11-29 16:05:20 -0800

    [ui.EqDots] Apply opacity in final step
    When in the final display step, apply some opacity to give substep
    indication of progress within the range rather than a hard cutoff

--- src/systemhud/ui/icons.py
+++ src/systemhud/ui/icons.py
@@ -19,12 +19,15 @@ def set_icon(icon: str) -> None:
 
 class EqDots:
     ICONS: Sequence[str] = "⡀⠄⠂⠁"
-    LEVELS: Sequence[int] = [0, 25, 50, 75]
-    COLORS: Sequence[str] = ["F00", "F60", "FF0", "0C0"]
+    LEVELS: Sequence[int] = [0, 25, 50, 75, 100]
+    COLORS: Sequence[str] = ["FF0000", "FF6600", "FFFF00", "00FF00"]
     ZERO_COLOR: str = "666"
 
     def __init__(
         self,
+        # the default being an empty list is me being lazy in typing, it keeps
+        # the typing consistent and the value is falsey which instead uses a
+        # default that *isn't* the shared reference
         levels: Sequence[int] = [],
         colors: Sequence[str] = [],
         zero_color: str = "",
@@ -38,11 +41,25 @@ class EqDots:
             return f"%{{F#{self.zero_color}}}{self.ICONS[0]}%{{F-}}"
 
         dots = ""
-        for threshold, color, icon in zip(self.levels, self.colors, self.ICONS):
-            if v > threshold:
+        for min_threshold, max_threshold, color, icon in \
+            zip(self.levels, self.levels[1:], self.colors, self.ICONS):
+            if v > max_threshold:
                 if dots:
                     dots += "%{O-11}"
                 dots += f"%{{F#{color}}}{icon}%{{F-}}"
+            elif v > min_threshold:
+                # when in the final threshold bracket, we apply an opacity
+                # to the dot, ranging from 50% to 100% (lower opacities were
+                # barely noticeable)
+                if dots:
+                    dots += "%{O-11}"
+                # basically 8*(percent between min and max) + 8
+                internal_bracket = int((v - min_threshold) * 8 / (max_threshold - min_threshold)) + 8
+                # The opacity needs to be 2 characters, I also uppercase it to
+                # be consistent with the other hex characters
+                opacity = hex(internal_bracket - 1)[-1].upper() * 2
+                dots += f"%{{F#{opacity}{color}}}{icon}%{{F-}}"
+                break
 
         return dots
 
